6129e05b915bc698cf8e8b23c89de6d35d1e2adf
maheshkolagatla <maheshkolagatla14@gmail.com>
2025-09-07 14:55:03 +0530

    project setup for test
    project setup for test

---
+++ SampleLatestApp/SampleLatestApp/SampleLatestAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  SampleLatestAppApp.swift
+//  SampleLatestApp
+//
+//  Created by mahesh kolagatla on 07/09/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SampleLatestAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ SampleLatestApp/SampleLatestAppTests/SampleLatestAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleLatestAppTests.swift
+//  SampleLatestAppTests
+//
+//  Created by mahesh kolagatla on 07/09/25.
+//
+
+import Testing
+@testable import SampleLatestApp
+
+struct SampleLatestAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
